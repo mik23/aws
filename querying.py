72589872c58997be23a08527d05b55ddc1ef2acc
Michael <marcotrigiano.m@gmail.com>
2021-05-22 06:44:13 +0100

    feat(): inserting

--- querying.py
+++ querying.py
@@ -5,7 +5,7 @@ import pymysql as mariadb
 
 rds_id = 'rds-db'
 db_name = 'rdsMariaDev'
-username_name = 'masteruser'
+user_name = 'masteruser'
 user_password = 'masterpassword'
 rds_endpoint = ''
 
@@ -13,4 +13,25 @@ rds_client = boto3.client('rds')
 response = rds_client.describe_db_instances(DBInstanceIdentifier=rds_id)
 
 rds_endpoint = json.dumps(response['DBInstances'][0]['Endpoint']['Address'])
-print(rds_endpoint)
+rds_endpoint = rds_endpoint.replace('"', '')
+
+db_connection = mariadb.connect(host=rds_endpoint,
+ user=user_name,
+ password=user_password, 
+ database=db_name)
+
+cursor = db_connection.cursor()
+
+try:
+    cursor.execute("CREATE TABLE ADS( ads_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, description VARCHAR(100))")
+    print("table created!")
+    
+sql = "INSERT INTO `ADS` (`description`) VALUES (%s)"
+cursor.execute(sql, ('ads about things'))
+cursor.execute(sql, ('ads about stuff'))
+db_connection.commit()
+    print('Iserted Data to Database!')
+except mariadb.Error as e:
+    print("Error: {}".format(e))
+finally:
+    db_connection.close()
